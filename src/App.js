5f024a721d700a1d7cea87bb0871d4454bda3580
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-17 18:32:45 +0530

    built the summary page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
+import Summary from './components/Layout/Summary';
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <Summary/>
     </Fragment>
   );
 }
