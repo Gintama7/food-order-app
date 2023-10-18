b9c46966a3ae2c977123891bebce59fe2a678b81
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-18 11:17:41 +0530

    added form for amount of meals select

--- src/components/Meals/MealItems.js
+++ src/components/Meals/MealItems.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './MealItems.css'
 
 const MealItems = (props) => {
-  return (
+  return (<section>
     <div className='meal_items'>      
         <h3>{props.name}</h3>
         <p>{props.desc}</p>
         <h2>${props.price}</h2>   
     </div>
+    <div className='mealform'>
+      <form action="">
+        <div className='form_input'>
+        <label htmlFor="amount">Amount</label>
+        <input type="text" placeholder='1' />
+        </div>        
+        <button className='add_btn'>+Add</button>
+      </form>
+    </div>
+    </section>
   )
 }
 
