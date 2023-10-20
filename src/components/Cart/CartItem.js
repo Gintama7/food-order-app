b3bcd4e923a2ec459840db3d93c5431aa0f1d7c5
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-20 11:41:02 +0530

    added styles to the cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -10,10 +10,11 @@ const CartItems = (props) => {
   return (
     <li className='cart_items'>
       <div>
-      <h3>{props.name}</h3></div>
-      <div className='cart_info'>
-      <span>${props.price}</span>
-      <span>Quantity{props.quantity}</span>
+         <h3>{props.name}</h3>  
+         <div className='cart_info'>    
+      <span className='price'>${props.price}</span>
+      <span className='quan'>x{props.quantity}</span>
+      </div>
       </div>
       <div className='buttons'>
         <button onClick={props.onAdd}>+</button>
